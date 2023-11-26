f2b15b951b0f95ec825f28b0bd3864ac4f7eeca3
GabbySuwichaya <s_suwanwimolkul@hotmail.com>
2023-11-27 02:07:45 +0700

    Update setting

--- cuee_scripts/Long-Exp/Linear-CUEE_Train.sh
+++ cuee_scripts/Long-Exp/Linear-CUEE_Train.sh
@@ -5,10 +5,10 @@ fi
 if [ ! -d "./logs/LongForecasting" ]; then
     mkdir ./logs/LongForecasting
 fi
-seq_len=16
+seq_len=128
 label_len=0
 model_name=DLinear
-for pred_len in 4
+for pred_len in 32 64 128 
 do
 python -u run_longExp.py \
   --is_training 1 \
@@ -24,7 +24,7 @@ python -u run_longExp.py \
   --pred_len $pred_len \
   --enc_in 1 \
   --des 'Exp' \
-  --itr 1 --batch_size 16  --learning_rate 0.005 --individual >logs/LongForecasting/$model_name'_I_'CUEE_$seq_len'_'$pred_len.log 
+  --itr 1 --batch_size 128  --learning_rate 0.005 --individual >logs/LongForecasting/$model_name'_I_'CUEE_$seq_len'_'$label_len'_'$pred_len.log 
  
 done
  
